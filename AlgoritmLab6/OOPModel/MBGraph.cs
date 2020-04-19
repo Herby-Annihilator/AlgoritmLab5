1f2cc5e1147d27909176a747e0ea511834f3c633
Danil Rukin <Rukin2018D@yandex.ru>
2020-04-19 11:58:01 +0700

    Начал делать лабу 6

---
+++ AlgoritmLab6/OOPModel/MBGraph.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AlgoritmLab5.AlgoritmLab6.OOPModel
+{
+    public class MBGraph : Graph<int>
+    {
+        public override void Print(TextWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
